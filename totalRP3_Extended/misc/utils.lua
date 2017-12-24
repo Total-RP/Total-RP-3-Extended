67209f0e5c7c2f7f7748b2d7052fcc5b35067f6c
Solanya <paul.corlay@gmail.com>
2017-12-24 10:39:14 +0100

    Better fix for coordinates
    I'm not sure why I didn't just do that in the first place, makes much more sense.

--- totalRP3_Extended/misc/utils.lua
+++ totalRP3_Extended/misc/utils.lua
@@ -27,18 +27,18 @@ local sqrt, pow = math.sqrt, math.pow;
 
 local function getUnitPositionSafe(unit)
 	local uY, uX, uZ, instanceID = UnitPosition(unit);	-- It's not a typo.
-	return uX or 0, uY or 0, uZ or 0, instanceID or "0";
+	return uY or 0, uX or 0, uZ or 0, instanceID or "0";
 end
 TRP3_API.extended.getUnitPositionSafe = getUnitPositionSafe;
 
 function TRP3_API.extended.unitDistancePoint(unit, x, y)
-	local uX, uY = getUnitPositionSafe(unit);
+	local uY, uX = getUnitPositionSafe(unit);
 	return sqrt(pow(x - uX, 2) + pow(y - uY, 2));
 end
 
 function TRP3_API.extended.unitDistanceMe(unit)
 	if not UnitExists(unit) then return 0; end
-	local uX, uY = getUnitPositionSafe(unit);
-	local mX, mY = getUnitPositionSafe("player");
+	local uY, uX = getUnitPositionSafe(unit);
+	local mY, mX = getUnitPositionSafe("player");
 	return sqrt(pow(mX - uX, 2) + pow(mY - uY, 2));
 end
