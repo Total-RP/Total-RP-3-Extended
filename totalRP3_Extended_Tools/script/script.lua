55cc39e1a2eeeb12b2f23ce9852e9e7763a7eac1
Solanya <17127080+Solanya@users.noreply.github.com>
2020-11-13 23:26:04 +0100

    Emote effect (#177)
    * #30 WIP on DoEmote effect, not tested
    
    * #30 Fix DoEmote implementation and UI
    
    * Dynamic categorisation for other emotes
    
    * Add unit test example
    
    * Fixed wrong dependency category for WoWUnit
    
    * Dynamic emote categories
    
    * Move imported locale keys
    
    * Accounting for special characters in alphabetical order
    
    * Unicode aware sorting function
    
    * YEET
    
    * Review fixes
    
    Co-authored-by: Renaud "Ellypse" Parize <ellypse@totalrp3.info>

--- totalRP3_Extended_Tools/script/script.lua
+++ totalRP3_Extended_Tools/script/script.lua
@@ -44,6 +44,7 @@ function TRP3_API.extended.tools.getEffectOperandLocale()
 			"speech_env",
 			"speech_npc",
 			"speech_player",
+			"do_emote",
 		},
 		[loc.REG_COMPANIONS] = {
 			"companion_dismiss_mount",
