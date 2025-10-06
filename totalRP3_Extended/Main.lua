d5301836b7a843cbc4e6e71620085d45375537ba
Solanya <solanya@totalrp3.info>
2025-10-06 20:09:11 +0200

    Version bump

--- totalRP3_Extended/Main.lua
+++ totalRP3_Extended/Main.lua
@@ -748,8 +748,8 @@ local function onStart()
     dashboard.extendedlogo:SetTexture("Interface\\AddOns\\totalRP3_Extended\\resources\\extendedlogooverlay");
 end
 
-Globals.extended_version = 1053;
-Globals.required_trp3_build = 143;
+Globals.extended_version = 1054;
+Globals.required_trp3_build = 145;
 
 --@debug@
 Globals.extended_display_version = "v-dev";
